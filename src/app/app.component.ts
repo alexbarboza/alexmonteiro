23dcb8d2a47ef863b0b1234fdaf491e2ad8e5b6e
Alex Monteiro <alexmonteirobarboza@gmail.com>
2018-04-11 10:39:53 -0300

    adicionado angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'app';
+// }
+import { Component } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
+})
+export class AppComponent {
+  items: Observable<any[]>;
+  constructor(db: AngularFirestore) {
+    this.items = db.collection('testes').valueChanges();
+  }
+}
