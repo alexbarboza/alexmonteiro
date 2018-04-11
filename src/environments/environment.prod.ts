1d5c3315553ac0f4e21ae1a04c9ebe4f62a71715
Alex Monteiro <alexmonteirobarboza@gmail.com>
2018-04-11 11:11:13 -0300

    configuração do angular-cli e firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBZDRQiPTlVf6tovI2kjmcWAL8jZXESVgo",
+    authDomain: "alexmonteiro-com-br.firebaseapp.com",
+    databaseURL: "https://alexmonteiro-com-br.firebaseio.com",
+    projectId: "alexmonteiro-com-br",
+    storageBucket: "alexmonteiro-com-br.appspot.com",
+    messagingSenderId: "311216067404"
+  }
 };
