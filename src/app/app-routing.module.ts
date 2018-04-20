5e7da6ed994bd95ae519fc4c7e4012617b875e80
Alex Monteiro <alexmonteirobarboza@gmail.com>
2018-04-20 09:43:05 -0300

    adicionado um template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
+import { NotFoundComponent } from './not-found.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: '',      redirectTo: 'login', pathMatch: 'full' },
-  { path: '**',    redirectTo: 'login' }
+  { path: '404', component: NotFoundComponent },
+  { path: '', component: HomeComponent },
+  { path: '**',    redirectTo: '404' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,3 @@
-// import { BrowserModule } from '@angular/platform-browser';
-// import { NgModule } from '@angular/core';
-
-
-// import { AppComponent } from './app.component';
-
-
-// @NgModule({
-//   declarations: [
-//     AppComponent
-//   ],
-//   imports: [
-//     BrowserModule
-//   ],
-//   providers: [],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -27,9 +9,9 @@ import { AngularFireAuthModule } from 'angularfire2/auth';
 import { environment } from '../environments/environment';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from './login/login.component';
-import { AppRoutingModule } from './/app-routing.module'; // https://ng-bootstrap.github.io/#/getting-started
-// import { RouterModule } from '@angular/Router';
-// import { routerConfig } from './router.config';
+import { AppRoutingModule } from './/app-routing.module';
+import { NotFoundComponent } from './not-found.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports: [
@@ -38,11 +20,11 @@ import { AppRoutingModule } from './/app-routing.module'; // https://ng-bootstra
     AngularFirestoreModule, // imports firebase/firestore, only needed for database features
     AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
     AngularFireStorageModule, // imports firebase/storage only needed for storage features
-    NgbModule.forRoot(), AppRoutingModule, // imports ng-bootstrap
-    // RouterModule.forRoot(routerConfig),
+    NgbModule.forRoot(), // imports ng-bootstrap
+    AppRoutingModule, 
   ],
   declarations: [ 
-    AppComponent, LoginComponent
+    AppComponent, LoginComponent, NotFoundComponent, HomeComponent
   ],
   bootstrap: [ AppComponent ]
 })
